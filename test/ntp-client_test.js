03c2d67859c07f0992589c950ffec4fbf69751af
Clément Bourgeois <moonpyk@gmail.com>
2013-07-08 23:16:52 +0200

    A few unit tests, I'm not sure if they are gonna work on Travis.

--- test/ntp-client_test.js
+++ test/ntp-client_test.js
@@ -21,15 +21,35 @@
 (function (exports) {
     "use strict";
 
-    var ntp_client = require('../lib/ntp-client.js');
+    var ntpClient = require('../lib/ntp-client.js');
 
     exports.setUp = function (done) {
         // setup here
         done();
     };
 
-    exports.noop = function (test) {
-        test.ok(true);
-        test.done();
+    exports.works = function (test) {
+        ntpClient.ntpReplyTimeout = 5000; // Reducing timeout to avoid warnings.
+
+        ntpClient.getNetworkTime(ntpClient.defaultNtpServer, ntpClient.defaultNtpPort, function (err, date) {
+            var now = new Date();
+            test.ok(err === null);
+            test.ok(date !== null);
+
+            test.equal(now.getDate(), date.getDate()); // I'm pretty sure the date things will be OK
+            test.equal(now.getMonth(), date.getMonth());
+            test.equal(now.getYear(), date.getYear());
+
+            // For the hours and minute parts, really depends if testing machine is synched
+            test.equal(now.getHours(), date.getHours());
+        });
+
+        // I'm pretty sure there is no NTP Server listening at google.com
+        ntpClient.getNetworkTime("google.com", 123, function (err, date) {
+            test.ok(err !== null);
+            test.ok(date === null);
+            test.equal(err, "Timeout waiting for NTP response.");
+            test.done();
+        });
     };
 }(exports));
